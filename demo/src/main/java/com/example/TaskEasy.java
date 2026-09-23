7489b63fd589aba4280f53b2079b8cdcf7b7ae1e
Trofim <b.trof1m777@gmail.com>
2026-09-23 12:28:51 +0000

    NewTasks

---
+++ demo/src/main/java/com/example/TaskEasy.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class TaskEasy {
+    public static void main(String[] args) {
+        //здесь вызывайте метод
+    }
+
+    //Необходимо написать программу, 
+    //которая выводит оценку в зависимости от баллов. 
+    //80–100 → «Отлично»
+    //60–79 → «Хорошо»
+    //40–59 → «Удовлетворительно»
+    //0–39 → «Неудовлетворительно»
+    public static String getAssignment(int points) {
+        return "";
+    }
+
+}
---
+++ demo/src/main/java/com/example/TaskHard.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class TaskHard {
+    public static void main(String[] args) {
+        String str = "Стэндофф";
+        String subString = "офф";
+        int index = str.indexOf(subString);
+        System.out.println(index);
+    }
+
+    // Задача (сложная): валидация email и пароля
+    // Написать два метода (можно и в один метод уместить):
+    // 1) Первый метод проверяет валидность email. 
+    //    Email должен содержать @. (подсказка в методе main этого класса) 
+    // 2) Второй метод отвечает за 
+    //     валидность пароля (минимум 8 символов)
+
+    public static boolean isValidData(String email, String password) {
+        
+        
+        return true;
+    }
+
+}
